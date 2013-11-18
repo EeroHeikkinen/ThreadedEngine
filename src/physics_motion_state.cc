b63b974c705588518d6d11d1105b29fc632ca20f
Saku2 <sakari.tanskanen@gmail.com>
2013-11-18 23:42:32 +0200

    merge

--- src/physics_motion_state.cc
+++ src/physics_motion_state.cc
@@ -1,6 +1,9 @@
 #include "physics_motion_state.hh"
 #include "physics_tree.hh"
 
+//TEMP
+#include <iostream>
+
 
 PhysicsMotionState::PhysicsMotionState(const btTransform& initialPos_, PhysicsComponent* component_) :
 									  initialPos(initialPos_), component(component_)
@@ -12,6 +15,7 @@ void PhysicsMotionState::setComponent(PhysicsComponent* component_) {
 
 void PhysicsMotionState::getWorldTransform(btTransform& worldTrans) const{
 	worldTrans = initialPos;
+	std::cout << "GETTER" << std::endl;
 }
 
 void PhysicsMotionState::setWorldTransform(const btTransform& worldTrans) {
