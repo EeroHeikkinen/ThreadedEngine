4992a14afa54b4327ef8f481eee125cf77b07b06
Antti Hatanmaa <antti.hatanmaa@aalto.fi>
2013-11-18 23:26:25 +0200

    vittusaatana

--- src/physics_motion_state.cc
+++ src/physics_motion_state.cc
@@ -1,10 +1,11 @@
 #include "physics_motion_state.hh"
 #include "physics_tree.hh"
+#include <iostream> //temp
 
 
 PhysicsMotionState::PhysicsMotionState(const btTransform& initialPos_, PhysicsComponent* component_) :
 									  initialPos(initialPos_), component(component_)
-									  {}
+									  {std::cout << "MotStatArgh: " << component << std::endl;}
 
 void PhysicsMotionState::setComponent(PhysicsComponent* component_) {
 	component = component_;
@@ -14,8 +15,24 @@ void PhysicsMotionState::getWorldTransform(btTransform& worldTrans) const{
 	worldTrans = initialPos;
 }
 
-void PhysicsMotionState::setWorldTransform(const btTransform& worldTrans) {
-	if (component == nullptr)
+void PhysicsMotionState::setWorldTransform(const btTransform& worldTrans){
+	if (!component){
+        std::cout << "NULULULULLLLUUUU" << std::endl;
 		return;
+	}
+	btMatrix3x3 B = worldTrans.getBasis();
+	btVector3 O = worldTrans.getOrigin();
+	std::cout << "Component: " << component << std::endl << std::endl;
+	std::cout << "Basis: " << std::endl;
+	for(size_t i = 0; i < 3; ++i){
+        for(size_t j = 0; j < 3; ++j)
+            std::cout << B[i][j] << " ";
+        std::cout << std::endl;
+	}
+	std::cout << std::endl;
+	std::cout << "Origin: ";
+    for(size_t i = 0; i < 3; ++i)
+        std::cout << O[i] << " ";
+    std::cout << std::endl << std::endl;
 	component->setTransformation(worldTrans);
 }
--- src/resource_thread.cc
+++ src/resource_thread.cc
@@ -51,9 +51,9 @@ void ResourceThread::init(void){
                                root,
                                glm::vec3(0.0f, 2.0f, 0.0f),
                                1.0f);
+
     pBox = new Test::Box(2.0f, 0.1f, 2.0f,
-                         new btSphereShape(1),
-                         //new btBoxShape(btVector3(1.0f, 0.1f, 1.0f)),
+                         new btBoxShape(btVector3(1.0f, 0.1f, 1.0f)),
                          root,
                          glm::vec3(0.0f, -2.0f, 0.0f),
                          2.0f);
